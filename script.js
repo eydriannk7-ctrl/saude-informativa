bf2f0ec7de95c1ca7128cfc67fb25d13bf72127d
eydriannk7-ctrl <eydrian.nk.7@gmail.com>
2026-02-26 16:33:41 -0300

    Adicionando seção completa de transtornos mentais com accordion

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ toggle.addEventListener("click", () => {
         toggle.textContent = "🌙";
     }
 });
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+accordionBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
+
+        if(content.style.display === "block"){
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
